f408c9c51e8bb22aca4dc179dda90262834c593f
Preethi Ranjit <rpsthecoder@users.noreply.github.com>
2017-07-14 20:23:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var templateContent = document.querySelector('template').content,
-    article = document.querySelector('article').cloneNode(true);
-article.querySelectorAll('*[id]').forEach((ele)=>{ele.removeAttribute('id')});
-article.attachShadow({  mode: 'closed' }).appendChild(templateContent.cloneNode(true));
-document.querySelector('#toc').appendChild(article);
+if('name' in document.createElement('slot') && 'attachShadow' in document.createElement('p')) {
+    let templateContent = document.querySelector('template').content;
+    let article = document.querySelector('article').cloneNode(true);
+    article.querySelectorAll('*[id]').forEach((ele)=>{ele.removeAttribute('id')})
+    article.attachShadow({  mode: 'closed' }).appendChild(templateContent.cloneNode(true));
+    document.querySelector('#toc').appendChild(article);
+}
+else{
+    let strong = document.createElement('strong');
+    strong.textContent = '⚠️ HTML Slot or Shadow DOM is not supported in your browser.';
+    strong.style['color'] = 'red';
+    document.body.insertBefore(strong, document.querySelector('h1'))
+}
